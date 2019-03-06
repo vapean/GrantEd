0b297ce70af6c9c5100d6ec9a4a1ecc63c05d02a
Antonio Vallejo <antonio.vapei@gmail.com>
2019-03-06 12:32:39 +0100

    registro de usuarios funcional

--- src/app/UsuarioCompleto/registro/registro.component.ts
+++ src/app/UsuarioCompleto/registro/registro.component.ts
@@ -17,27 +17,21 @@ export class RegistroComponent implements OnInit {
   arrProvinces: string[]
 
   constructor(private usuarioService: UsuariosService) {
-    this.arrCountries = ['Todos', 'Alemania', 'Andorra', 'Angola', 'Arabia Saudita', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaiyán', 'Bahrein', 'Bangladesh', 'Bélgica', 'Belice', 'Bolivia', 'Bosnia y Herzegovina', 'Brasil', 'Brunei', 'Bulgaria', 'Camboya', 'Camerún', 'Canadá', 'Catar', 'Chile', 'China', 'Chipre', 'Ciudad del Vaticano', 'Colombia', 'Corea del Sur', 'Costa Rica', 'Croacia', 'Cuba', 'Dinamarca', 'Dominica', 'Ecuador', 'Egipto', 'El Salvador', 'Emiratos Árabes Unidos', 'Escocia', 'Eslovaquia', 'Eslovenia', 'España', 'Estados Unidos', 'Estonia', 'Etiopía', 'Filipinas', 'Finlandia', 'Francia', 'Georgia', 'Ghana', 'Grecia', 'Guatemala', 'Guyana', 'Haití', 'Honduras', 'Hong Kong', 'Hungría', 'India', 'Indonesia', 'Irak', 'Irán', 'Irlanda', 'Islandia', 'Israel', 'Italia', 'Jamaica', 'Japón', 'Jordania', 'Kazajistán', 'Kenia', 'Kirguistán', 'Letonia', 'Líbano', 'Libia', 'Liechtenstein', 'Lituania', 'Luxemburgo', 'Macedonia', 'Malasia', 'Maldivas', 'Malí', 'Malta', 'Marruecos', 'México', 'Mozambique', 'Nepal', 'Nicaragua', 'Nigeria', 'Noruega', 'Nueva Zelanda', 'Online', 'Países Bajos', 'Pakistán', 'Panamá', 'Paraguay', 'Perú', 'Polonia', 'Portugal', 'Puerto Rico', 'Reino Unido', 'República Checa', 'República Dominicana', 'Rumania', 'Rusia', 'Senegal', 'Serbia', 'Singapur', 'Siria', 'Sri Lanka', 'Sudáfrica', 'Suecia', 'Suiza', 'Tailandia', 'Taiwan', 'Tanzania', 'Túnez', 'Turquía', 'Ucrania', 'Uganda', 'Uruguay', 'Venezuela', 'Vietnam']
-    
-    this.arrProvinces = ['Todos', 'A Coruña', 'Álava', 'Albacete', 'Alicante', 'Almería', 'Asturias', 'Ávila', 'Badajoz', 'Baleares', 'Barcelona', 'Burgos', 'Cáceres', 'Cádiz', 'Cantabria', 'Castellón', 'Ciudad Real', 'Córdoba', 'Cuenca', 'Girona', 'Granada', 'Guadalajara', 'Gipuzkoa', 'Huelva', 'Huesca', 'Jaén', 'La Rioja', 'Las Palmas', 'León', 'Lérida', 'Lugo', 'Madrid', 'Málaga', 'Murcia', 'Navarra', 'Ourense', 'Palencia', 'Pontevedra', 'Salamanca', 'Segovia', 'Sevilla', 'Soria', 'Tarragona', 'Santa Cruz de Tenerife', 'Teruel', 'Toledo', 'Valencia', 'Valladolid', 'Vizcaya', 'Zamora', 'Zaragoza']
-    
+    this.arrCountries = ['Alemania', 'Andorra', 'Angola', 'Arabia Saudita', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaiyán', 'Bahrein', 'Bangladesh', 'Bélgica', 'Belice', 'Bolivia', 'Bosnia y Herzegovina', 'Brasil', 'Brunei', 'Bulgaria', 'Camboya', 'Camerún', 'Canadá', 'Catar', 'Chile', 'China', 'Chipre', 'Ciudad del Vaticano', 'Colombia', 'Corea del Sur', 'Costa Rica', 'Croacia', 'Cuba', 'Dinamarca', 'Dominica', 'Ecuador', 'Egipto', 'El Salvador', 'Emiratos Árabes Unidos', 'Escocia', 'Eslovaquia', 'Eslovenia', 'España', 'Estados Unidos', 'Estonia', 'Etiopía', 'Filipinas', 'Finlandia', 'Francia', 'Georgia', 'Ghana', 'Grecia', 'Guatemala', 'Guyana', 'Haití', 'Honduras', 'Hong Kong', 'Hungría', 'India', 'Indonesia', 'Irak', 'Irán', 'Irlanda', 'Islandia', 'Israel', 'Italia', 'Jamaica', 'Japón', 'Jordania', 'Kazajistán', 'Kenia', 'Kirguistán', 'Letonia', 'Líbano', 'Libia', 'Liechtenstein', 'Lituania', 'Luxemburgo', 'Macedonia', 'Malasia', 'Maldivas', 'Malí', 'Malta', 'Marruecos', 'México', 'Mozambique', 'Nepal', 'Nicaragua', 'Nigeria', 'Noruega', 'Nueva Zelanda', 'Online', 'Países Bajos', 'Pakistán', 'Panamá', 'Paraguay', 'Perú', 'Polonia', 'Portugal', 'Puerto Rico', 'Reino Unido', 'República Checa', 'República Dominicana', 'Rumania', 'Rusia', 'Senegal', 'Serbia', 'Singapur', 'Siria', 'Sri Lanka', 'Sudáfrica', 'Suecia', 'Suiza', 'Tailandia', 'Taiwan', 'Tanzania', 'Túnez', 'Turquía', 'Ucrania', 'Uganda', 'Uruguay', 'Venezuela', 'Vietnam']
+
+    this.arrProvinces = ['A Coruña', 'Álava', 'Albacete', 'Alicante', 'Almería', 'Asturias', 'Ávila', 'Badajoz', 'Baleares', 'Barcelona', 'Burgos', 'Cáceres', 'Cádiz', 'Cantabria', 'Castellón', 'Ciudad Real', 'Córdoba', 'Cuenca', 'Girona', 'Granada', 'Guadalajara', 'Gipuzkoa', 'Huelva', 'Huesca', 'Jaén', 'La Rioja', 'Las Palmas', 'León', 'Lérida', 'Lugo', 'Madrid', 'Málaga', 'Murcia', 'Navarra', 'Ourense', 'Palencia', 'Pontevedra', 'Salamanca', 'Segovia', 'Sevilla', 'Soria', 'Tarragona', 'Santa Cruz de Tenerife', 'Teruel', 'Toledo', 'Valencia', 'Valladolid', 'Vizcaya', 'Zamora', 'Zaragoza']
+
     this.arrLevels = ['Concursos', 'Curso corto', 'Doctorado', 'Formación Profesional', 'Fellowship', 'Idiomas', 'Máster', 'Postdoctorado', 'Práctica', 'Grado Universitario', 'Residencia artística', 'Otros']
-    
+
     this.arrFields = ['Acústica', 'Administración de Empresas', 'Administración Publica', 'Alemán', 'Arquitectura', 'Artes', 'Astronomía', 'Bellas Artes', 'Biología', 'Botánica', 'Chino', 'Ciencias Agrícolas', 'Ciencias Naturales', 'Ciencias Políticas', 'Cine', 'Cirugía', 'Coaching', 'Comercio Exterior', 'Comunicación', 'Contaduria', 'Coreano', 'Derecho', 'DIseño', 'Ecología y Medio Ambiente', 'Economía', 'Educación', 'Emprendimiento', 'Energías Alternativas', 'Especialidades en Medicina', 'Estadística', 'Estudios Bibliográficos', 'Estucios Culturales', 'Estudios del Desarrollo', 'Farmacología', 'Filología / Letras', 'Finanzas', 'Físisa', 'Fotografia', 'Frances', 'Gastronomñia', 'Genética', 'Gestión de eventos', 'Griego', 'Hebreo', 'Historia', 'Hosteleria y Turismo', 'Humanidades', 'Ingeniería', 'Inglés', 'Italiano', 'Japones', 'Marketing Digital', 'Matemáticas', 'Medicina', 'Meditación', 'Moda', 'Música', 'Neurociencia', 'Oftafmología', 'Periodismo', 'Portugués', 'Psicología', 'Psiquiatría', 'Química', 'Relaciones Internacionales', 'Salud Pública', 'Sociología', 'Sostenibilidad', 'Tecnología e ingormática', 'Turismo', 'Urbanismo', 'Veterinaria', 'Zoología']
-    
-    
-  
 
 
-
-    
-    
   }
 
   ngOnInit() {
     this.formRegistro = new FormGroup(
       {
-        nombre: new FormControl("Antonio", [
+        name: new FormControl("Antonio", [
           Validators.required,
           Validators.maxLength(15)
         ]),
@@ -61,14 +55,26 @@ export class RegistroComponent implements OnInit {
           Validators.required,
           Validators.maxLength(20)
         ]),
-
+        study_field: new FormControl("", []),
+        study_level: new FormControl("", []),
+        country_origin: new FormControl("", [
+          Validators.required
+        ]),
+        province_destination: new FormControl("", []),
+        province_origin: new FormControl("", [
+          Validators.required
+        ]),
+        country_destination: new FormControl("", []),
+        date: new FormControl("", []),
+        gender: new FormControl("", [
+        ]),
         age: new FormControl("19", [this.ageValidator])
       },
       [this.passwordRepeatValidator]
       // este va a nivel del formulario general proque tiene que acceder a mas de un imput
     );
 
-    
+
   }
 
   ngAfterViewInit() {
@@ -80,28 +86,36 @@ export class RegistroComponent implements OnInit {
     if (isNaN(control.value) == true) {
       return { age: "Debe ser un numero" };
     }
-
     if (control.value < 18 || control.value > 99) {
       return { age: "La edad debe estar entre 18 y 99" };
     }
-
     return null;
   }
 
   passwordRepeatValidator(group: FormGroup) {
     let password = group.controls["password"].value;
     let password_repeat = group.controls["password_repeat"].value;
-
     return password == password_repeat
       ? null
       : { repeatpassword: "Las contraseñas deben ser iguales" };
   }
 
   manejarFormulario() {
-    this.usuarioService
-      .enviarFormulario(this.formRegistro.value)
-      .subscribe(res => {
-        console.log(res);
-      });
+    // console.log(this.formRegistro.value)
+
+    this.formRegistro.value.study_field = this.formRegistro.value.study_field.join(',')
+    console.log(this.formRegistro.value)
+    this.formRegistro.value.study_level = this.formRegistro.value.study_level.join(',')
+    console.log(this.formRegistro.value)
+    this.formRegistro.value.country_destination = this.formRegistro.value.country_destination.join(',')
+    console.log(this.formRegistro.value)
+    this.formRegistro.value.province_destination = this.formRegistro.value.province_destination.join(',')
+    console.log(this.formRegistro.value)
+
+    this.usuarioService.enviarFormulario(this.formRegistro.value).subscribe(res => {
+      console.log(res);
+    });
   }
+
+
 }
--- src/app/usuarios.service.ts
+++ src/app/usuarios.service.ts
@@ -7,13 +7,18 @@ import { HttpClient } from '@angular/common/http';
 export class UsuariosService {
   url: string
   constructor(private httpClient: HttpClient) {
-    this.url= "http://registrate.ngrok.io/"
+    //   this.url= "http://registrate.ngrok.io/"
+    // }
+    this.url = "http://localhost:3000/api/usuario/new"
   }
   
-
   enviarFormulario(form) {
-    return this.httpClient.post(`${this.url}register`, {'name': form.nombre, 'surname':form.surname, 'username':form.username, 'password':form.password, 'mail':form.mail, 'address':form.adress, 'age':form.age})
+    return this.httpClient.post(`${this.url}`, {'name': form.name, 'surname':form.surname, 'username':form.username, 'password':form.password, 'email':form.mail, 'address':form.adress, 'age':form.age, 'country_origin':form.country_origin,'province_origin':form.province_origin, 'country_destination':form.country_destination,'province_destination':form.province_destination, 'study_field':form.study_field, 'study_level':form.study_level, 'date':form.date, 'gender':form.gender,})
   }
+
+  // enviarFormulario(form) {
+  //   return this.httpClient.post(`${this.url}register`, {'name': form.nombre, 'surname':form.surname, 'username':form.username, 'password':form.password, 'mail':form.mail, 'address':form.adress, 'age':form.age})
+  // }
 
   loginFormulario(form) {
     return this.httpClient.post(`${this.url}login`, { 'username': form.username, 'password': form.password,})
